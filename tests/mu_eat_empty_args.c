469f9a418464ed1e589f0142bd9c3514008e37f3
Andrei Porumb <anporumb@microsoft.com>
2019-03-31 01:08:25 -0700

    nicer variable names

--- tests/mu_eat_empty_args.c
+++ tests/mu_eat_empty_args.c
@@ -80,119 +80,120 @@ int run_mu_eat_empty_args_test(void)
     /*see that it eats / doesn't eat from 2 arguments*/
 
     { /*scope because of repetitive naure of tests and copy&paste*/
-        int testerArray_1[] = { MU_EAT_EMPTY_ARGS(42 + 1,) }; /*eats 1 empty argument, expands to "42+1"*/
-        POOR_MANS_ASSERT_AND_RETURN(1 == (sizeof(testerArray_1) / sizeof(testerArray_1[0])));
-        POOR_MANS_ASSERT_AND_RETURN(42 + 1 == testerArray_1[0]);
+        int testerArray[] = { MU_EAT_EMPTY_ARGS(42 + 1,) }; /*eats 1 empty argument, expands to "42+1"*/
+
+        POOR_MANS_ASSERT_AND_RETURN(1 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(42 + 1 == testerArray[0]);
     }
 
     { /*scope because of repetitive naure of tests and copy&paste*/
-        int testerArray_2[] = { MU_EAT_EMPTY_ARGS(42 + 2,) }; /*eats 1 empty argument, expands to "42+1"*/
-        POOR_MANS_ASSERT_AND_RETURN(1 == (sizeof(testerArray_2) / sizeof(testerArray_2[0])));
-        POOR_MANS_ASSERT_AND_RETURN(42 + 2 == testerArray_2[0]);
+        int testerArray[] = { MU_EAT_EMPTY_ARGS(,42 + 2) }; /*eats 1 empty argument, expands to "42+2"*/
+
+        POOR_MANS_ASSERT_AND_RETURN(1 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(42 + 2 == testerArray[0]);
     }
     
     {
-        int testerArray_3[] = { MU_EAT_EMPTY_ARGS(42 + 3 ,42 + 4) }; /*doesn't eat anything*/
+        int testerArray[] = { MU_EAT_EMPTY_ARGS(42 + 3 ,42 + 4) }; /*doesn't eat anything*/
 
-        POOR_MANS_ASSERT_AND_RETURN(2 == (sizeof(testerArray_3) / sizeof(testerArray_3[0])));
-        
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_3[0] == 42 + 3);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_3[1] == 42 + 4);
+        POOR_MANS_ASSERT_AND_RETURN(2 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[0] == 42 + 3);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[1] == 42 + 4);
     }
 
     /*see that it eats / doesn't eat from 3 arguments*/
     {
-        int testerArray_4[] = { MU_EAT_EMPTY_ARGS(42 + 5 ,42 + 6, 42 + 7) }; /*doesn't eat anything*/
+        int testerArray[] = { MU_EAT_EMPTY_ARGS(42 + 5 ,42 + 6, 42 + 7) }; /*doesn't eat anything*/
 
-        POOR_MANS_ASSERT_AND_RETURN(3 == (sizeof(testerArray_4) / sizeof(testerArray_4[0])));
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[0] == 42 + 5);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[1] == 42 + 6);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[2] == 42 + 7);
+        POOR_MANS_ASSERT_AND_RETURN(3 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[0] == 42 + 5);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[1] == 42 + 6);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[2] == 42 + 7);
     }
 
     /*see that it eats / doesn't eat from 3 arguments*/
     {
-        int testerArray_4[] = { MU_EAT_EMPTY_ARGS( ,42 + 6, 42 + 7) }; /*eats first*/
+        int testerArray[] = { MU_EAT_EMPTY_ARGS( ,42 + 6, 42 + 7) }; /*eats first*/
 
-        POOR_MANS_ASSERT_AND_RETURN(2 == (sizeof(testerArray_4) / sizeof(testerArray_4[0])));
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[0] == 42 + 6);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[1] == 42 + 7);
+        POOR_MANS_ASSERT_AND_RETURN(2 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[0] == 42 + 6);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[1] == 42 + 7);
     }
 
     /*see that it eats / doesn't eat from 3 arguments*/
     {
-        int testerArray_4[] = { MU_EAT_EMPTY_ARGS(42 + 5 ,, 42 + 7) }; /*eat middle*/
+        int testerArray[] = { MU_EAT_EMPTY_ARGS(42 + 5 ,, 42 + 7) }; /*eat middle*/
 
-        POOR_MANS_ASSERT_AND_RETURN(2 == (sizeof(testerArray_4) / sizeof(testerArray_4[0])));
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[0] == 42 + 5);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[1] == 42 + 7);
+        POOR_MANS_ASSERT_AND_RETURN(2 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[0] == 42 + 5);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[1] == 42 + 7);
     }
 
     /*see that it eats / doesn't eat from 3 arguments*/
     {
-        int testerArray_4[] = { MU_EAT_EMPTY_ARGS(42 + 5 ,42 + 6, ) }; /*eats last*/
+        int testerArray[] = { MU_EAT_EMPTY_ARGS(42 + 5 ,42 + 6, ) }; /*eats last*/
 
-        POOR_MANS_ASSERT_AND_RETURN(2 == (sizeof(testerArray_4) / sizeof(testerArray_4[0])));
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[0] == 42 + 5);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[1] == 42 + 6);
+        POOR_MANS_ASSERT_AND_RETURN(2 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[0] == 42 + 5);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[1] == 42 + 6);
     }
 
     /*see that it eats / doesn't eat from 3 arguments*/
     {
-        int testerArray_4[] = { MU_EAT_EMPTY_ARGS(42 + 5 , , ) }; /*eats middle and last*/
+        int testerArray[] = { MU_EAT_EMPTY_ARGS(42 + 5 , , ) }; /*eats middle and last*/
 
-        POOR_MANS_ASSERT_AND_RETURN(1 == (sizeof(testerArray_4) / sizeof(testerArray_4[0])));
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[0] == 42 + 5);
+        POOR_MANS_ASSERT_AND_RETURN(1 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[0] == 42 + 5);
     }
 
     /*see that it eats / doesn't eat from 3 arguments*/
     {
-        int testerArray_4[] = { MU_EAT_EMPTY_ARGS(,42 + 6, ) }; /*eats first and last*/
+        int testerArray[] = { MU_EAT_EMPTY_ARGS(,42 + 6, ) }; /*eats first and last*/
 
-        POOR_MANS_ASSERT_AND_RETURN(1 == (sizeof(testerArray_4) / sizeof(testerArray_4[0])));
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[0] == 42 + 6);
+        POOR_MANS_ASSERT_AND_RETURN(1 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[0] == 42 + 6);
     }
 
     /*see that it eats / doesn't eat from 3 arguments*/
     {
-        int testerArray_4[] = { MU_EAT_EMPTY_ARGS(, , 42 + 7) }; /*eats first and middle*/
+        int testerArray[] = { MU_EAT_EMPTY_ARGS(, , 42 + 7) }; /*eats first and middle*/
 
-        POOR_MANS_ASSERT_AND_RETURN(1 == (sizeof(testerArray_4) / sizeof(testerArray_4[0])));
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[0] == 42 + 7);
+        POOR_MANS_ASSERT_AND_RETURN(1 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[0] == 42 + 7);
     }
 
 
     /*see that it eats / doesn't eat from 10 arguments*/
     {
-        int testerArray_4[] = { MU_EAT_EMPTY_ARGS (1,2,3,4,5,6,7,8,9,10)}; /*eats nothing*/
+        int testerArray[] = { MU_EAT_EMPTY_ARGS (1,2,3,4,5,6,7,8,9,10)}; /*eats nothing*/
 
-        POOR_MANS_ASSERT_AND_RETURN(10 == (sizeof(testerArray_4) / sizeof(testerArray_4[0])));
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[0] == 1);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[1] == 2);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[2] == 3);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[3] == 4);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[4] == 5);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[5] == 6);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[6] == 7);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[7] == 8);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[8] == 9);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[9] == 10);
+        POOR_MANS_ASSERT_AND_RETURN(10 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[0] == 1);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[1] == 2);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[2] == 3);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[3] == 4);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[4] == 5);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[5] == 6);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[6] == 7);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[7] == 8);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[8] == 9);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[9] == 10);
     }
 
     /*see that it eats / doesn't eat from 10 arguments*/
     {
-        int testerArray_4[] = { MU_EAT_EMPTY_ARGS(1,2, ,4,5,6,7,8,9,10) }; /*eats 3*/
+        int testerArray[] = { MU_EAT_EMPTY_ARGS(1,2, ,4,5,6,7,8,9,10) }; /*eats 3*/
 
-        POOR_MANS_ASSERT_AND_RETURN(9 == (sizeof(testerArray_4) / sizeof(testerArray_4[0])));
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[0] == 1);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[1] == 2);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[2] == 4);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[3] == 5);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[4] == 6);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[5] == 7);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[6] == 8);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[7] == 9);
-        POOR_MANS_ASSERT_AND_RETURN(testerArray_4[8] == 10);
+        POOR_MANS_ASSERT_AND_RETURN(9 == (sizeof(testerArray) / sizeof(testerArray[0])));
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[0] == 1);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[1] == 2);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[2] == 4);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[3] == 5);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[4] == 6);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[5] == 7);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[6] == 8);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[7] == 9);
+        POOR_MANS_ASSERT_AND_RETURN(testerArray[8] == 10);
     }
 
     return 0;

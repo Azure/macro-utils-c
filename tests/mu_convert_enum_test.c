17d85659215153b9b430288efbd5cabf18f6199d
Dan Cristoloveanu <dcristo@microsoft.com>
2020-03-23 09:47:11 -0700

    Fix detection of using enum values that are not part of the enum

--- tests/mu_convert_enum_test.c
+++ tests/mu_convert_enum_test.c
@@ -11,7 +11,7 @@
 // This lists the values in the first enum
 #define TEST_CONVERT_FROM_ENUM_VALUES \
     test_from_a, \
-    test_from_b, \
+    test_from_b = 42, /* Assigning a value to the enum used to fail ..., now fixed */ \
     test_from_c
 
 #define TEST_CONVERT_TO_ENUM_VALUES \
@@ -29,7 +29,6 @@ MU_DEFINE_ENUM(TEST_CONVERT_TO_ENUM, TEST_CONVERT_TO_ENUM_VALUES);
 // Also the following code should not compile because it uses a from value that is not part of the enum :
 /*MU_DEFINE_CONVERT_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_TO_ENUM,
     test_from_a, test_to_a,
-    test_from_b, test_to_a,
     42, test_to_a,
     test_from_c, test_to_b);*/
 

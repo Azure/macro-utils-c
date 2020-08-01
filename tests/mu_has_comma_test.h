c312055e64ec0b8837efefff64f1ee5dc33d0f6e
Andrei Porumb <anporumb@microsoft.com>
2020-07-31 21:43:11 -0700

    2 lines were reversed

--- tests/mu_has_comma_test.h
+++ tests/mu_has_comma_test.h
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#define MU_HAS_COMMA_TEST_H
 #ifndef MU_HAS_COMMA_TEST_H
+#define MU_HAS_COMMA_TEST_H
 
 int run_mu_has_comma_tests(void);
 

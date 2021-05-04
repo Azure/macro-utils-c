44591cd4d70ff7386476881f5d589fd2f98dc75c
anporumb@microsoft.com <anporumb@microsoft.com>
2021-05-04 15:47:27 -0700

    after CR and harcoding time_t ascii representation to 24 characters

--- inc/macro_utils/macro_utils.h
+++ inc/macro_utils/macro_utils.h
@@ -217,11 +217,11 @@ const char* MU_C3(MU_, enumIdentifier,_ToString)(enumIdentifier value)          
 #define MU_TRUE_STRING "true"
 #define MU_BOOL_VALUE(v) MU_FALSE_STRING "\0" MU_TRUE_STRING + ((!!(v))*sizeof(MU_FALSE_STRING)), (v)
 
-/*PRI_TIME_T is to be used with a time_t variable - it produces strings such as "Tue May  4 14:42:17 2021"*/
-#define PRI_TIME_T "*.*s"
+/*PRI_TIME_T is to be used with a time_t variable - it produces strings such as "Tue May  4 14:42:17 2021". As per 7.27.3.1 The asctime function of C11 it seems that limiting the output to 24 characters should be safe.*/
+#define PRI_TIME_T ".24s"
 
 /*TIME_T_VALUE is the counterpar of PRI_TIME_T in a printf/LogError instruction*/
-#define TIME_T_VALUE(t) (int)strlen(ctime(&t))-1, (int)strlen(ctime(&t))-1, ctime(&t)
+#define TIME_T_VALUE(t) ctime(&t)
 
 /*PRI_MU_ENUM and MU_ENUM_VALUE/MU_ENUM_VALUE_2 work together as printf format specifier/argument. e.g: printf("enumValue was=%" PRI_MU_ENUM "\n", MU_ENUM_TO_STRING(enumIdentifier, enumValue));*/
 #define PRI_MU_ENUM "s%s (%d)"
---
+++ tests/mu_pri_time_t_test.h
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef MU_PRI_TIME_T_TEST_H
+#define MU_PRI_TIME_T_TEST_H
+
+int run_mu_pri_time_t_tests(void);
+
+#endif // MU_PRI_TIME_T_TEST_H

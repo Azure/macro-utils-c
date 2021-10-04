35334257f340ebf733d57f7bc70eddfd21e00a03
Dan Cristoloveanu <dcristo@microsoft.com>
2021-10-04 13:15:44 -0700

    Fix mu convert enum to allow converting from a "from" enum to 2 different "to" enums (#81)
    * Have MU_CONVERT_ENUM allow multiple conversions
    
    * Add test for converting a from enum to 2 different to enums
    
    * Add some more test code

--- tests/mu_convert_enum_test.c
+++ tests/mu_convert_enum_test.c
@@ -14,12 +14,19 @@
     test_from_b = 42, /* Assigning a value to the enum used to fail ..., now fixed */ \
     test_from_c
 
+MU_DEFINE_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_FROM_ENUM_VALUES);
+
 #define TEST_CONVERT_TO_ENUM_VALUES \
     test_to_a, \
     test_to_b
 
-MU_DEFINE_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_FROM_ENUM_VALUES);
 MU_DEFINE_ENUM(TEST_CONVERT_TO_ENUM, TEST_CONVERT_TO_ENUM_VALUES);
+
+#define TEST_CONVERT_TO_ANOTHER_ENUM_VALUES \
+    test_to_another_a, \
+    test_to_another_b
+
+MU_DEFINE_ENUM(TEST_CONVERT_TO_ANOTHER_ENUM, TEST_CONVERT_TO_ANOTHER_ENUM_VALUES);
 
 // The following code should not compile because there is a missing test_from_b in the from values :
 /*MU_DEFINE_CONVERT_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_TO_ENUM,
@@ -36,6 +43,12 @@ MU_DEFINE_CONVERT_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_TO_ENUM,
     test_from_a, test_to_a,
     test_from_b, test_to_b,
     test_from_c, test_to_b);
+
+// This has to compile :-) to prove that we can map the same from enum to 2 different to enums
+MU_DEFINE_CONVERT_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_TO_ANOTHER_ENUM,
+    test_from_a, test_to_another_a,
+    test_from_b, test_to_another_b,
+    test_from_c, test_to_another_b);
 
 int run_mu_convert_enum_tests(void)
 {
@@ -57,6 +70,29 @@ int run_mu_convert_enum_tests(void)
     POOR_MANS_ASSERT(result_value == test_to_b);
 
     convert_result = MU_CONVERT_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_TO_ENUM)(424242, &result_value);
+    POOR_MANS_ASSERT(convert_result != 0);
+
+    convert_result = MU_CONVERT_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_TO_ENUM)(424242, &result_value);
+    POOR_MANS_ASSERT(convert_result != 0);
+
+    TEST_CONVERT_TO_ANOTHER_ENUM another_result_value;
+
+    convert_result = MU_CONVERT_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_TO_ANOTHER_ENUM)(test_from_a, &another_result_value);
+    POOR_MANS_ASSERT(convert_result == 0);
+    POOR_MANS_ASSERT(another_result_value == test_to_another_a);
+
+    convert_result = MU_CONVERT_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_TO_ANOTHER_ENUM)(test_from_b, &another_result_value);
+    POOR_MANS_ASSERT(convert_result == 0);
+    POOR_MANS_ASSERT(another_result_value == test_to_another_b);
+
+    convert_result = MU_CONVERT_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_TO_ANOTHER_ENUM)(test_from_c, &another_result_value);
+    POOR_MANS_ASSERT(convert_result == 0);
+    POOR_MANS_ASSERT(another_result_value == test_to_another_b);
+
+    convert_result = MU_CONVERT_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_TO_ANOTHER_ENUM)(424242, &another_result_value);
+    POOR_MANS_ASSERT(convert_result != 0);
+
+    convert_result = MU_CONVERT_ENUM(TEST_CONVERT_FROM_ENUM, TEST_CONVERT_TO_ANOTHER_ENUM)(424242, &another_result_value);
     POOR_MANS_ASSERT(convert_result != 0);
 
     return result;

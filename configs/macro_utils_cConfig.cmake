88035c9c8aff07fd64f21cca4b08c6061da1ea73
anporumb <anporumb@users.noreply.github.com>
2024-10-14 11:11:38 -0700

    update for inc folder (#234)

--- configs/macro_utils_cConfig.cmake
+++ configs/macro_utils_cConfig.cmake
@@ -1,8 +1,6 @@
 #Copyright (c) Microsoft. All rights reserved.
 #Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-set(MACRO_UTILS_INC_FOLDER "${CMAKE_CURRENT_LIST_DIR}/include" CACHE INTERNAL "this is what needs to be included if using macro utils" FORCE)
-
 if("${CMAKE_VERSION}" VERSION_GREATER 3.0.2)
     include("${CMAKE_CURRENT_LIST_DIR}/macro_utils_cTargets.cmake")
 
@@ -10,5 +8,5 @@ if("${CMAKE_VERSION}" VERSION_GREATER 3.0.2)
 
     set(MACRO_UTILS_C_INCLUDES ${MACRO_UTILS_C_INCLUDES} CACHE INTERNAL "")
 else()
-    message(STATUS "This version of CMake does not support interface targets. To use Azure Macro Utils, simply add \"MACRO_UTILS_INC_FOLDER\" to your include directories as specified in the instructions on the GitHub README.")
+    message(STATUS "This version of CMake does not support interface targets. Update CMake")
 endif()

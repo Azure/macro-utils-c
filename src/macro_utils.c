b3eaf1a37b1c9c9dc1a4f4027a101b1ece984627
Andrei Porumb <anporumb@microsoft.com>
2025-11-06 11:39:27 -0800

    remove unused macro_utils.c

--- src/macro_utils.c
+++
@@ -1,4 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-static int something_not_to_have_the_file_empty = 33;
